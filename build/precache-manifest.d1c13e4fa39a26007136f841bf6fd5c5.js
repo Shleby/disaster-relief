1c53e4bf01cf53332e0af60827cd71abfee0e6e4
Shelby Huffman <shelby.a.huffy@gmail.com>
2019-11-02 18:43:28 -0500

    add: build folder

---
+++ build/precache-manifest.d1c13e4fa39a26007136f841bf6fd5c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328e03a9efc388cde80f9d4c3ebc079c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ee9863e0cde62733a3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b5b04720973b53d0315",
+    "url": "/static/js/2.48dc28ac.chunk.js"
+  },
+  {
+    "revision": "c1ee9863e0cde62733a3",
+    "url": "/static/js/main.3093c110.chunk.js"
+  },
+  {
+    "revision": "2180cca06c9e05cdd69c",
+    "url": "/static/js/runtime-main.29573a70.js"
+  }
+]);
