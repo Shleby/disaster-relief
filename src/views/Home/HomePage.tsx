ecf4787d1d4cc2b3ec0fe426b3e9d207c798cb4d
Shelby Huffman <shelby.a.huffy@gmail.com>
2019-11-03 04:06:36 -0600

    whoops

--- src/views/Home/HomePage.tsx
+++ src/views/Home/HomePage.tsx
@@ -3,19 +3,20 @@ import * as React from "react";
 import "./HomePage.css";
 import disasters from "../../disasters.json";
 import Category from "../../components/Category";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class MainApp extends React.Component<any, any> {
   render() {
     return (
       <div className="cover">
+        <div></div>
         <div className="container">
           <p className="containerTitle">Provide disaster relief for...</p>
           {disasters.map(function(i: any) {
-            return(
+            return (
               <div>
-                <Link to='/labels'>
-                  <Category name={i.name} type={i.type}/>
+                <Link to="/labels">
+                  <Category name={i.name} type={i.type} />
                 </Link>
               </div>
             );
