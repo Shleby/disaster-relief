4e0f03c6662f41536de6238b94c4fef61a7fe324
Shelby Huffman <shelby.a.huffy@gmail.com>
2019-11-03 01:46:06 -0500

    add: stitch folder

--- src/views/Home/HomePage.tsx
+++ src/views/Home/HomePage.tsx
@@ -5,25 +5,32 @@ import disasters from "../../disasters.json";
 import Category from "../../components/Category";
 
 class MainApp extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      disaster: [],
+      name: "",
+      type: "",
+      date: "",
+      location: ""
+    };
+  }
+
   render() {
     return (
       <div className="cover">
         <div className="container">
-<<<<<<< HEAD
+          <p className="containerTitle">Provide disaster relief for...</p>
           {disasters.map(function(i: any) {
             return (
               <div>
-                <Category name={i.name} />
-=======
-          <p className="containerTitle">Provide disaster relief for...</p>
-          {disasters.map(function(i: any) {
-            return(
-              <div>
-                <Category name={i.name} type={i.type}/>
->>>>>>> origin/sam
+                <Category name={i.name} type={i.type} />
               </div>
             );
           })}
+          {this.state.disaster.map((disaster: { name: React.ReactNode }) => (
+            <div>{disaster.name}</div>
+          ))}
         </div>
       </div>
     );
--- src/views/MainApp.tsx
+++ src/views/MainApp.tsx
@@ -6,13 +6,24 @@ import HomePage from "./Home/HomePage";
 import LabelPage from "./Label/LabelPage";
 
 class MainApp extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      disasters: [],
+      name: "",
+      type: "",
+      date: "",
+      location: ""
+    };
+  }
+
   render() {
     return (
       <Router>
         <div className="background">
           <h1 className="title">cers</h1>
           <p className="subtitle">Centralized Emergency Releif System</p>
-          <LabelPage />
+          <HomePage />
         </div>
       </Router>
     );
